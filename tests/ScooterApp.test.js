793d3df6dc8fe57b29fbdc0ad669a64f4985b183
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2024-09-15 20:33:13 -0400

    saved

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -5,17 +5,17 @@ const scooterApp = new ScooterApp();
 // ScooterApp tests here
 
 // register user
-describe("registerUser method tests", () => {
-  test("Should return instance of User", () => {
-    const response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
-    expect(response).toBeInstanceOf(User);
-  });
-  // log in
-  test("test of login", () => {
-    const user = new User("Joe Bloggs", "test123");
-    expect(user.login()).toBe("Joe Bloggs", "test123");
-  });
-});
+// describe("registerUser method tests", () => {
+//   test("Should return instance of User", () => {
+//     const response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+//     expect(response).toBeInstanceOf(User);
+//   });
+// log in
+// test("test of login", () => {
+//   const user = new User("Joe Bloggs", "test123");
+//   expect(user.login()).toBe("Joe Bloggs", "test123");
+// });
+//});
 
 // log out
 
